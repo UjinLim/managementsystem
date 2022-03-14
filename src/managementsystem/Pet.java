f8d628cdc6f254477d97afbd86cff5744dc1c585
UjinLim <limwjjang@naver.com>
2022-03-14 23:11:55 +0900

    added pet selling system.menu

--- src/managementsystem/Pet.java
+++ src/managementsystem/Pet.java
@@ -3,7 +3,12 @@ package managementsystem;
 public class Pet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//애완동물 분양 관리 시스템을 만들고자함.
+		System.out.println("Add Pet");
+		System.out.println("View Pet");
+		System.out.println("Edit Pet");
+		System.out.println("Sell Pet");
+		System.out.println("Exit");
 
 	}
 
