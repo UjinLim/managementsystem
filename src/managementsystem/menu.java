cbf42c87f7f106db51a591bf05cb2a878adb7131
UjinLim <limwjjang@naver.com>
2022-03-14 17:18:21 +0900

    menu

---
+++ src/managementsystem/menu.java
@@ -0,0 +1,16 @@
+package managementsystem;
+
+public class menu {
+
+	public static void main(String[] args) {
+		System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");
+		
+		
+		
+	}
+
+}
